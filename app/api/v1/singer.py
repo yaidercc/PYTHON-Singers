8967bdd4d067fab9ed772b7f00160660a479397c
yaider <yaiderc19@gmail.com>
2025-09-16 12:47:43 -0500

    feat: getSinger by id service ready

--- app/api/v1/singer.py
+++ app/api/v1/singer.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas.singer import SingerCreate, SingerUpdate, SingerOut
-from app.services.singer import get_all
+from app.services.singer import get_all, get_by_id
 from app.db.session import get_db
 
 router = APIRouter(prefix="/singers", tags=["Singers"])
@@ -10,3 +10,11 @@ router = APIRouter(prefix="/singers", tags=["Singers"])
 @router.get("/", response_model=List[SingerOut])
 def getAllSingers(db: Session = Depends(get_db)):
     return get_all(db)
+
+
+@router.get("/{singer_id}", response_model=SingerOut)
+def getSinger(singer_id:int,db: Session = Depends(get_db)):
+    singer = get_by_id(db,singer_id)
+    if not singer:
+        raise HTTPException(status_code=404, detail="Singer not found")
+    return singer
--- app/services/singer.py
+++ app/services/singer.py
@@ -4,3 +4,6 @@ from app.schemas.singer import SingerCreate, SingerUpdate
 
 def get_all(db: Session):
     return db.query(Singer).all()
+
+def get_by_id(db: Session, singer_id: int):
+    return db.query(Singer).filter(Singer.id==singer_id).first()
